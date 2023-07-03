024b564317557786896c12e1d03ef02aeba66562
Quazi Hasnat Irfan <80903120+Irfan-IIUC@users.noreply.github.com>
2023-07-03 13:30:19 +0600

    Update Creation and Traversal.c

--- Linked List/Singly Linked List/Creation and Traversal.c
+++ Linked List/Singly Linked List/Creation and Traversal.c
@@ -1,11 +1,10 @@
 /*
-There are three common types of Linked List.
+There are three common types of Linked List. They are,
 -> Singly Linked List
 -> Doubly Linked List
 -> Circular Linked List
 
-A singly linked list is a linear data structure in which the elements are not stored in contiguous
-memory locations and each element is connected only to its next element using a pointer.
+A singly linked list is a linear data structure in which the elements are not stored in contiguous memory locations and each element is connected only to its next element using a pointer.
 */
 
 #include<stdio.h>
